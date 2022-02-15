7446de2f67b455fd4c21d311af4358ee680951be
fantasticnaveen <89866958+fantasticnaveen@users.noreply.github.com>
2022-02-15 16:49:35 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -48,9 +48,9 @@ function draw() {
 
 function blow() {
 
-  Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0, y:-0.05});
+  //Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0, y:-0.05});
 
-  //Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0, y:0.05});
+  Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0, y:0.05});
   
   //Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0.05, y:0.05});
   
